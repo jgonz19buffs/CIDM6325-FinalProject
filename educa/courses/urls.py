9f1fb8e234f693fc19af23ca05eaabc1bba1f8d3
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-10-29 11:42:40 -0500

    adding content to course modules

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -3,14 +3,29 @@ from . import views
 
 urlpatterns = [
     path(
-        'mine/',
-        views.ManageCourseListView.as_view(),
-        name='manage_course_list'
+        'content/<int:id>/delete/',
+        views.ContentDeleteView.as_view(),
+        name='module_content_delete'
     ),
     path(
         'create/',
         views.CourseCreateView.as_view(),
         name='course_create'
+    ),
+    path(
+        'mine/',
+        views.ManageCourseListView.as_view(),
+        name='manage_course_list'
+    ),
+    path(
+        'module/<int:module_id>/content/<model_name>/create/',
+        views.ContentCreateUpdateView.as_view(),
+        name='module_content_create'
+    ), 
+    path(
+        'module/<int:module_id>/content/<model_name>/<id>/',
+        views.ContentCreateUpdateView.as_view(),
+        name='module_content_update'
     ),
     path(
         '<pk>/edit/',
