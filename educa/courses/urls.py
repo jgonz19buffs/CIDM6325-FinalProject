9e77cc4b98692b149ce0d62519a4513f1596252c
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-10-29 11:05:13 -0500

    using formsets for course modules

--- educa/courses/urls.py
+++ educa/courses/urls.py
@@ -21,5 +21,10 @@ urlpatterns = [
         '<pk>/delete/',
         views.CourseDeleteView.as_view(),
         name='course_delete'
-    )
+    ),
+    path(
+        '<pk>/module/',
+        views.CourseModuleUpdateView.as_view(),
+        name='course_module_update'
+    ),
 ]
