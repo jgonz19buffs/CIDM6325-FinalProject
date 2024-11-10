3141c811007f9721f31fec82ba24f6fbb5c3ac10
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-11-10 17:02:13 -0600

    building custom API views

--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -1,9 +1,12 @@
 from rest_framework import generics
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from courses.api.pagination import StandardPagination
 from courses.api.serializers import CourseSerializer, SubjectSerializer
 from courses.models import Course, Subject
 from django.db.models import Count
+from django.shortcuts import get_object_or_404
 
 
 # class SubjectListView(generics.ListAPIView):
@@ -24,3 +27,9 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related('modules')
     serializer_class = CourseSerializer
     pagination_class = StandardPagination
+
+class CourseEnrollView(APIView):
+    def post(self, request, pk, format=None):
+        course = get_object_or_404(Course, pk=pk)
+        course.students.add(request.user)
+        return Response({'enrolled': True})
