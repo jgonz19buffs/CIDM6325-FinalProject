329d3d6c86816f5bdf806773719707f363c779cd
Joshua Gonzalez <jgonzalez19@buffs.wtamu.edu>
2024-11-10 17:45:38 -0600

    serializing course contents

--- educa/courses/api/serializers.py
+++ educa/courses/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from courses.models import Course, Module, Subject
+from courses.models import Content, Course, Module, Subject
 from django.db.models import Count
 
 class SubjectSerializer(serializers.ModelSerializer):
@@ -45,3 +45,33 @@ class CourseSerializer(serializers.ModelSerializer):
             'modules'
         ]
 
+class ItemRelatedField(serializers.RelatedField):
+    def to_representation(self, value):
+        return value.render()
+
+class ContentSerializer(serializers.ModelSerializer):
+    item = ItemRelatedField(read_only=True)
+    class Meta:
+        model = Content
+        fields = ['order', 'item']
+
+class ModuleWithContentsSerializer(serializers.ModelSerializer):
+    contents = ContentSerializer(many=True)
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description', 'contents']
+
+class CourseWithContentsSerializer(serializers.ModelSerializer):
+    modules = ModuleWithContentsSerializer(many=True)
+    class Meta:
+        model = Course
+        fields = [
+            'id',
+            'subject',
+            'title',
+            'slug',
+            'overview',
+            'created',
+            'owner',
+            'modules'
+        ]
--- educa/courses/api/views.py
+++ educa/courses/api/views.py
@@ -6,7 +6,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from courses.api.pagination import StandardPagination
-from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.api.permissions import IsEnrolled
+from courses.api.serializers import CourseSerializer, CourseWithContentsSerializer, SubjectSerializer
 from courses.models import Course, Subject
 from django.db.models import Count
 from django.shortcuts import get_object_or_404
@@ -32,7 +33,7 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     pagination_class = StandardPagination
 
     @action(
-        details=True,
+        detail=True,
         methods=['post'],
         authentication_classes=[BasicAuthentication],
         permission_classes=[IsAuthenticated]
@@ -41,6 +42,17 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
         course = self.get_object()
         course.students.add(request.user)
         return Response({'enrolled': True})
+    
+    @action(
+        detail=True,
+        methods=['get'],
+        serializer_class=CourseWithContentsSerializer,
+        authentication_classes=[BasicAuthentication],
+        permission_classes=[IsAuthenticated, IsEnrolled]
+    )
+    def contents(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
 
 # class CourseEnrollView(APIView):
 #     authentication_classes = [BasicAuthentication]
